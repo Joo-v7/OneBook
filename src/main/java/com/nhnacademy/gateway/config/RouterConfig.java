ebe4a6d14224264f10b75ecb8e83f9ddeccfab64
Joo-v7 <helloworld.v7@gmail.com>
2024-12-17 20:58:23 +0900

    .

---
+++ src/main/java/com/nhnacademy/gateway/config/RouterConfig.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterConfig {
+
+}
