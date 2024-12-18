0216dd4f192ad6edefee5dadb21c019b721ea8c1
Joo <125026282+Joo-v7@users.noreply.github.com>
2024-12-18 13:53:37 +0900

    Revert "."

--- src/main/java/com/nhnacademy/gateway/config/RouterConfig.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.gateway.config;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RouterConfig {
-
-}
