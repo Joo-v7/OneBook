3ecd7abe7a6778ee0ad071ee32a85487579202e5
bbangho <dudgh614@gmail.com>
2024-12-26 13:34:17 +0900

    error message

--- src/main/java/com/nhnacademy/gateway/filter/JwtAuthFilter.java
+++ src/main/java/com/nhnacademy/gateway/filter/JwtAuthFilter.java
@@ -1,10 +1,7 @@
 package com.nhnacademy.gateway.filter;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import com.nhnacademy.gateway.parser.OneBookJwtParser;
+import io.jsonwebtoken.*;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
@@ -12,9 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import javax.crypto.SecretKey;
-import java.security.Key;
 
 /**
  * jwt token을 인가하는 filter임
@@ -27,6 +21,11 @@ import java.security.Key;
 @Component
 public class JwtAuthFilter implements GlobalFilter, Ordered {
 
+    private final OneBookJwtParser oneBookJwtParser;
+
+    public JwtAuthFilter(OneBookJwtParser oneBookJwtParser) {
+        this.oneBookJwtParser = oneBookJwtParser;
+    }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -42,16 +41,27 @@ public class JwtAuthFilter implements GlobalFilter, Ordered {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
+
         String token = authHeader.substring(7);
         // 실제 JWT 검증 로직 필요 (signature, 만료시간, 클레임 등)
-        if(!validateToken(token)) {
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
+
+        String id = null;
+        try{
+            id = validateToken(token);
+
+            String finalId = id;
+            exchange = exchange.mutate()
+                    .request(builder -> builder.header("X-USER-ID", finalId))
+                    .build();
+        }catch (ExpiredJwtException e) {
+            return handleUnauthorized(exchange, "JWT token is expired");
+        } catch (UnsupportedJwtException e) {
+            return handleUnauthorized(exchange, "JWT token is unsupported");
+        } catch (MalformedJwtException e) {
+            return handleUnauthorized(exchange, "Malformed JWT token");
+        } catch (Exception e) {
+            return handleUnauthorized(exchange, "Invalid JWT token");
         }
-        // header에 추가하기
-        exchange = exchange.mutate()
-                .request(builder -> builder.header("X-USER-ID", "test"))
-                .build();
 
         return chain.filter(exchange);
     }
@@ -61,28 +71,30 @@ public class JwtAuthFilter implements GlobalFilter, Ordered {
         return -1; // 우선순위를 높게 주고 싶다면 음수 값 지정
     }
 
-    private boolean validateToken(String token) {
+    private String  validateToken(String token) {
         // TODO jwt 검증
         System.out.println("token : {}" + token);
-        // token 넘어오는거 확인했고
 
-        SecretKey key = Keys.hmacShaKeyFor("Ny0pm2CWIAST07ElsTAVZgCqJKJd2bE9lpKyewuOhyyKoBApt1Ny0pm2CWIAST07ElsTAVZgCqJKJd2bE9lpKyewuOhyyKoBApt1".getBytes());
-        //
-        Claims body = Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
+        Claims body = oneBookJwtParser.getJwtParser().
+                parseClaimsJws(token)
                 .getBody();
 
 
         if( body != null){
             System.out.println("body !! " + body.toString());
+            return (String)body.get("id");
         }
 
+        throw new RuntimeException();
+    }
 
+    private Mono<Void> handleUnauthorized(ServerWebExchange exchange, String message){
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        exchange.getResponse().getHeaders().add("Content-Type", "application/json");
+        String response = "{\"error\": \"Unauthorized\", \"message\": \"" + message + "\"}";
 
-        // jwt token에 담겨있는 것들 대충 expired ,
-
-        return true;
+        return exchange.getResponse().writeWith(
+                Mono.just(exchange.getResponse().bufferFactory().wrap(response.getBytes()))
+        );
     }
 }
